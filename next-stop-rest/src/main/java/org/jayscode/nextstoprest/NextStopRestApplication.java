69e7fcaa71b58646b88c386c340caceec13dcb50
Jaylen Thomas <jaylenthomas@Jaylens-MacBook-Pro.local>
2025-11-25 12:54:30 -0500

    Normalize line endings

---
+++ next-stop-rest/src/main/java/org/jayscode/nextstoprest/NextStopRestApplication.java
@@ -0,0 +1,13 @@
+package org.jayscode.nextstoprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextStopRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextStopRestApplication.class, args);
+    }
+
+}
---
+++ next-stop-rest/src/test/java/org/jayscode/nextstoprest/NextStopRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jayscode.nextstoprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextStopRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
