c72a27a6f8901b527ff4a66a92864b14a13b71a7
Jaylen Thomas <93702389+Jaylen412@users.noreply.github.com>
2025-11-27 14:05:38 -0500

    service file

---
+++ next-stop-rest/src/main/java/org/jayscode/nextstoprest/service/PredictionService.java
@@ -0,0 +1,9 @@
+package org.jayscode.nextstoprest.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PredictionService {
+}
