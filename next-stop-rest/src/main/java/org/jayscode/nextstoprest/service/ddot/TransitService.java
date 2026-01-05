f40f166672884bab796202fa98ee4787467059d4
Jaylen Thomas <93702389+Jaylen412@users.noreply.github.com>
2026-01-05 15:02:27 -0500

    Transit service edit

--- next-stop-rest/src/main/java/org/jayscode/nextstoprest/service/ddot/TransitService.java
+++ next-stop-rest/src/main/java/org/jayscode/nextstoprest/service/ddot/TransitService.java
@@ -1,6 +1,7 @@
 package org.jayscode.nextstoprest.service.ddot;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jayscode.nextstoprest.model.ddot.Vehicle;
 import org.jayscode.nextstoprest.repo.ddot.ArrivalEstimateRepo;
 import org.jayscode.nextstoprest.repo.ddot.RouteRepo;
 import org.jayscode.nextstoprest.repo.ddot.StopRepo;
@@ -8,14 +9,16 @@ import org.jayscode.nextstoprest.repo.ddot.VehicleRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Slf4j
 @Service
 public class TransitService {
 
-    private ArrivalEstimateRepo arrivalEstimateRepo;
-    private RouteRepo routeRepo;
-    private StopRepo stopRepo;
-    private VehicleRepo vehicleRepo;
+    private final ArrivalEstimateRepo arrivalEstimateRepo;
+    private final RouteRepo routeRepo;
+    private final StopRepo stopRepo;
+    private final VehicleRepo vehicleRepo;
 
     public TransitService(@Autowired ArrivalEstimateRepo arrivalEstimateRepo,
                           @Autowired RouteRepo routeRepo,
